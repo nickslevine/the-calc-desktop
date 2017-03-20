2c032625c9fb16e8ca94d9b4a5ac73dfe7aa9d77
Nick Levine <nickslevine@gmail.com>
2017-03-20 22:26:28 +0000

    first commit

---
+++ calc.js
@@ -0,0 +1,34 @@
+//Interface
+function calc() {
+        if (document.getElementById('expression').value != '') {
+        exp = document.getElementById('expression').value;
+        ans = eval(exp);
+        console.log(exp, " ", ans);
+        if (ans != undefined) {
+        document.getElementById('answers').innerHTML+= exp;
+        document.getElementById('answers').innerHTML+= '<span class="answertext">\t=\t';
+        document.getElementById('answers').innerHTML+= '<b>' + ans + '</b></span>';
+        document.getElementById('answers').innerHTML+= '<br><br>';
+        document.getElementById('expression').value = ans;
+        } else {
+            document.getElementById('expression').value = '';
+        }
+        }
+    }
+    function focbox() {
+        document.getElementById('expression').focus();
+    }
+    function clear() {
+        document.getElementById('expression').value = '';
+        console.log('worked');
+    }
+    // Math functions
+    function divide() {
+        document.getElementById('expression').value += '/';
+    }
+    function multiply() {
+        document.getElementById('expression').value += '*';
+    }
+    function btninput(val) {
+        document.getElementById('expression').value += val;
+    }
